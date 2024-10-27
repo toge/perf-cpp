7a7a081dc9b093ff29894850fd86b5a5a55f1aa4
Jan Mühlig <jan.muehlig@tu-dortmund.de>
2024-10-27 15:42:34 +0100

    Added comment for feature.h

--- include/perfcpp/feature.h
+++ include/perfcpp/feature.h
@@ -2,27 +2,34 @@
 
 #include <linux/version.h>
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,2,0)
+/// The features of the perf subsystem have evolved over time (more precisely over Linux Kernel generations).
+/// In this file, we define some preprocessor variables to keep up with older Linux Kernel versions without yielding
+/// errors at compile- and runtime.
+/// The documentation for the perf_event_open system call (https://man7.org/linux/man-pages/man2/perf_event_open.2.html)
+/// has a great overview of features added in various versions.
+/// For the moment, we support Linux 4.0 and newer.
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 2, 0)
 #define PERFCPP_NO_SAMPLE_BRANCH_IND_JUMP
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,3,0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 3, 0)
 #define PERFCPP_NO_RECORD_SWITCH
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,4,0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 4, 0)
 #define PERFCPP_NO_SAMPLE_BRANCH_CALL
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,8,0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 8, 0)
 #define PERFCPP_NO_SAMPLE_MAX_STACK
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,13,0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 13, 0)
 #define PERFCPP_NO_SAMPLE_PHYS_ADDR
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,17,0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 17, 0)
 #define PERFCPP_NO_RECORD_MISC_SWITCH_OUT_PREEMPT
 #endif
 
